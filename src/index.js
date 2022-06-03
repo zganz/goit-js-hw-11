ea5f8e1b2bbc96aa301031f1ab1918cb13723978
zganz <humanmailer@gmail.com>
2022-06-03 14:39:07 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './sass/main.scss';
 import Notiflix from 'notiflix';
 import "notiflix/dist/notiflix-3.2.5.min.css";
+import renderData from './js/render';
 const axios = require('axios');
 let page = 0;
 let query = "";
@@ -9,19 +10,20 @@ let rootUrl = 'https://pixabay.com/api/?key=27763232-d5fad278e4d8773c17239879d&i
 const searchForm = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
 const loadMoreBt = document.querySelector('.load-more');
+loadMoreBt.style.display = 'none';
 loadMoreBt.addEventListener('click', onLoadMore);
 
 searchForm.addEventListener('submit', onSearchFormSubmit);
 
 function onLoadMore(){
 page = page+1;
-getPictures(query,page).then(data => {        
-  if (data.totalHits === 0) {
+  getPictures(query,page).then(data => {        
+      if (data.totalHits === 0) {
       
       return Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
   };       
-  
-  renderData (data.hits);  
+    
+    renderData (data.hits,gallery);  
 })
   .catch(error => errorHandling());  
 }
@@ -29,36 +31,16 @@ getPictures(query,page).then(data => {
 function onSearchFormSubmit(event) {
     event.preventDefault();
     query = event.target.elements["searchQuery"].value;
-  
+    if (query.trim() === '') {
+      gallery.innerHTML = '';
+      Notiflix.Notify.failure("Не корректный ввод");
+      return false;
+  }; 
+    loadMoreBt.style.display = 'block';
     onLoadMore();
 }
 
-function renderData (data){
-    console.log(data);
-    data.forEach(function(element){
-      
-      let item =`<div class="photo-card">
-        <img src="${element.webformatURL}" alt="" loading="lazy" class='gallery__image' />
-        <div class="info">
-          <p class="info-item">
-            <b>Likes -"${element.likes}"</b>
-          </p>
-          <p class="info-item">
-            <b>Views - ${element.views}</b>
-          </p>
-          <p class="info-item">
-            <b>Comments - ${element.comments}</b>
-          </p>
-          <p class="info-item">
-            <b>Downloads - ${element.downloads}</b>
-          </p>
-        </div>
-      </div>`;
-      gallery.insertAdjacentHTML("beforeend", item);
-    });
-    
-      
-}
+
 
 async function getPictures(query,page) {
   let request = rootUrl + '&q='+query+'&page='+ page;
---
+++ src/js/render.js
@@ -0,0 +1,26 @@
+export default function renderData (data,gallery){
+    console.log(data);
+    data.forEach(function(element){
+        
+      let item =`<div class="photo-card">
+        <img src="${element.webformatURL}" alt="" loading="lazy" class='gallery__image' />
+        <div class="info">
+          <p class="info-item">
+            <b>Likes -"${element.likes}"</b>
+          </p>
+          <p class="info-item">
+            <b>Views - ${element.views}</b>
+          </p>
+          <p class="info-item">
+            <b>Comments - ${element.comments}</b>
+          </p>
+          <p class="info-item">
+            <b>Downloads - ${element.downloads}</b>
+          </p>
+        </div>
+      </div>`;
+      gallery.insertAdjacentHTML("beforeend", item);
+    });
+    
+    
+}
